0f3b8bc95f5e87a83cef7e0d5bf4bea56a606413
Ana <dia151378@uvg.edu.gt>
2019-09-09 22:46:21 -0600

    reciben cartas 2 jugadores
    tienen que apachar enter  y luego j cada vez que ingrese un nuevo cliente

--- server.py
+++ server.py
@@ -53,17 +53,17 @@ def handle_client(client):
             random.shuffle(cartas)
             print("REVOLVIENDO CARTAS...",cartas)
             print(contador)
+            jugador1 = cartas[0:4]
+            jugador1 = (' '.join(jugador1))
+            jugador2 = cartas[4:8]
+            jugador2 = (' '.join(jugador2))
             if contador == 1:
+                client.send(bytes(jugador1,"utf8" ))
+                print("jugador1", jugador1)
                 print("necesitas mas jugadores")
             if contador == 2:
-                jugador1 = cartas[0:4]
-                jugador1 = (' '.join(jugador1))
-                print("jugador1", jugador1)
-                client.send(bytes(jugador1,"utf8" ))
-                jugador2 = cartas[4:8]
-                jugador2 = (' '.join(jugador2))
                 print("jugador2", jugador2)
-                client.send(bytes(jugador1,"utf8" ))
+                client.send(bytes(jugador2,"utf8" ))
             if contador == 3:
                 jugador1 = cartas[0:4]
                 print("jugador1", jugador1)
